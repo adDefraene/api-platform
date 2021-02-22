7bffdce5ee9fc0910ffeb14b5da11fee5b23879a
adDefraene <adriendefraene@gmail.com>
2021-02-22 11:48:59 +0100

    Lifecycle Kernel - Customer/User

---
+++ src/Events/CustomerUserSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use App\Entity\Customer;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class CustomerUserSubscriber implements EventSubscriberInterface
+{
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['setUserForCustomer', EventPriorities::PRE_VALIDATE]
+        ];
+    }
+
+    public function setUserForCustomer(ViewEvent $event)
+    {
+        $customer = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        //VERIFY IF THE REQUEST IS A POST CUSTOMER
+        if($customer instanceof Customer && $method === "POST")
+        {
+            //GET THE CONNECTED USER
+            $user = $this->security->getUser();
+            $customer->setUser($user);
+        }
+    }
+}
